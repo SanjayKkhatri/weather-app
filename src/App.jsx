f08742282f5d2957b36d291db6f707f7a5f61aa2
SanjayKkhatri <khatrisanjay095@gmail.com>
2022-09-23 12:02:31 -0700

    Weather App

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [userCity, setUserCity] = useState("");
+
+  useEffect()
+
+  function handleForm(e) {
+    e.preventDefault();
+    console.log("inputValue : ", inputValue);
+
+    if(!inputValue){
+      console.log("Filled is empty...");
+      return;
+    }
+  }
+  return (
+    <div className="weather-app">
+      <form onSubmit={handleForm}>
+      <input type="text" placeholder='Enter City Name...' value={inputValue}
+      onChange={(e)=> {setInputValue(e.target.value)}} 
+      />
+      </form>
+    </div>
+  );
+}
+
+export default App;
